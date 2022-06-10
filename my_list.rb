a25b229dfa8d6ee3a429df1854fb6e629dcd310f
isaacpitwa <isaacpitwa256@gmail.com>
2022-06-10 22:38:00 +0300

    Erase Testing code

--- my_list.rb
+++ my_list.rb
@@ -11,10 +11,3 @@ class MyList
     @list.each(&block)
   end
 end
-
-list = MyList.new(1, 2, 3, 4)
-list.all? { |e| e < 5 }
-list.all? { |e| e > 5 }
-list.any? { |e| e == 2 }
-list.any? { |e| e == 5 }
-list.filter(&:even?)
